1a20820048ed10e0a3f146f8d1274c73dd978eee
54blackghost <ulrichleblack@gmail.com>
2026-02-20 10:31:59 +0100

    refactoring with the build

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Detect user language
+const userLang = navigator.language || navigator.languages[0];
+const shortLang = userLang.split("-")[0];
+
+// Set HTML lang attribute
+document.documentElement.lang = shortLang;
+
 createRoot(document.getElementById("root")!).render(<App />);
